eac74a65ca4f836c45a44a490ab6050edc239ec3
Nicholas Padilla <nicholaspadilla@NicholassMBP10.home>
2024-05-13 09:38:14 -0500

    Change app.py hostname to 0.0.0.0

--- app/app.py
+++ app/app.py
@@ -3,4 +3,4 @@ from application import Application
 
 if __name__ == '__main__':
     app = create_api(__name__, Application())
-    app.run(host="localhost", port=5000)
+    app.run(host="0.0.0.0", port=5000)
