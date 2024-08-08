4bf60231183ba1ed69c98113b8eb27d458bd9cdd
Nicholas Padilal <nicholas.padilla@npcomplete.io>
2024-08-08 10:59:33 -0500

    add auth decorator

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
+from httplib2 import Credentials
 from api import create_api
 from application import Application
+from scratch import firebase_login
 
 if __name__ == '__main__':
+    # Initialize the firebase application
+    cred = Credentials.Certificate('creds/firebase-cred.json')
+    firebase_login.initialize_app(cred)
+
+    # Initialize Flask app
     app = create_api(__name__, Application())
     app.run(host="0.0.0.0", port=5000)
